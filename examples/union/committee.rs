47dd6546936d974617d76669b1a4a4277dcf8dd6
Agustin Zavalla <azavalla@dc.uba.ar>
2025-06-20 12:10:43 -0300

    Remove unused params

--- examples/union/committee.rs
+++ examples/union/committee.rs
@@ -185,7 +185,7 @@ impl Member {
         })
     }
 
-    pub fn prepare_drp(&mut self, covenant_id: Uuid, member1: &Member, member2: &Member, addresses: &Vec<P2PAddress>, utxo: &Utxo) -> Result<()> {
+    pub fn prepare_drp(&mut self, covenant_id: Uuid, member1: &Member, member2: &Member) -> Result<()> {
         info!(id = self.id, "Preparing DRP covenant {} for {} and {}", covenant_id, member1.id, member2.id);
 
         // Get the pairwise aggregated key for this pair
@@ -216,11 +216,6 @@ impl Member {
         let txid_str = "0000000000000000000000000000000000000000000000000000000000000000";
         let txid = bitcoin::Txid::from_str(txid_str)
             .map_err(|e| anyhow::anyhow!("Failed to parse txid: {}", e))?;
-
-        // Get the pairwise aggregated key for this pair
-        // let counterparty_address = self.get_counterparty_address(member1, member2)?;
-        // let pair_aggregated_pub_key = self.keyring.pairwise_keys.get(&counterparty_address)
-            // .ok_or_else(|| anyhow::anyhow!("Pairwise key not found for counterparty: {:?}", counterparty_address))?;
 
         let initial_utxo = Utxo::new(txid, 4, 200_000, pair_aggregated_pub_key);
         let prover_win_utxo = Utxo::new(txid, 2, 10_500, pair_aggregated_pub_key);
@@ -253,7 +248,6 @@ impl Member {
             VariableTypes::Utxo((prover_win_utxo.txid, prover_win_utxo.vout, Some(prover_win_utxo.amount), Some(prover_win_output_type)))
         )?;
 
-        sleep(Duration::from_secs(20));
         Ok(())
     }
 
@@ -422,8 +416,7 @@ impl Member {
     }
 
     fn setup_drp_covenant(&mut self, members: &Vec<Member>, utxo: &Utxo, session_id: Uuid) -> Result<()> {
-        // TODO is this the right numbers of drps? we are skipping when both are challengers, but
-        // why do challengers need to create drps?
+        // TODO we don't need to create the B => A covenant when B is a challenger
         let my_address = self.address()?.clone();
 
         // Create a sorted list of members to have a canonical order of pairs.
@@ -453,7 +446,7 @@ impl Member {
                     let name_to_hash = format!("drp_covenant:{:?}:{:?}:{:?}", op1_address, op2_address, session_id);
                     let covenant_id_1 = Uuid::new_v5(&namespace, name_to_hash.as_bytes());
                     let participants_1 = vec![op1_address.clone(), op2_address.clone()];
-                    self.prepare_drp(covenant_id_1, member1, member2, &self.get_addresses(members), utxo)?;
+                    self.prepare_drp(covenant_id_1, member1, member2)?;
                     self.bitvmx.setup(covenant_id_1, PROGRAM_TYPE_DRP.to_string(), participants_1, 0)?;
 
                     let other_address_1 = self.get_counterparty_address(member1, member2)?;
@@ -468,7 +461,7 @@ impl Member {
                     let name_to_hash = format!("drp_covenant:{:?}:{:?}:{:?}", op2_address, op1_address, session_id);
                     let covenant_id_2 = Uuid::new_v5(&namespace, name_to_hash.as_bytes());
                     let participants_2 = vec![op2_address.clone(), op1_address.clone()];
-                    self.prepare_drp(covenant_id_2, member2, member1, &self.get_addresses(members), utxo)?;
+                    self.prepare_drp(covenant_id_2, member2, member1)?;
                     self.bitvmx.setup(covenant_id_2, PROGRAM_TYPE_DRP.to_string(), participants_2, 0)?;
 
                     self.covenants.drp_covenants.push(DrpCovenant {
