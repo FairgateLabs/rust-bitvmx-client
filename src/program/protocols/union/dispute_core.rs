1abd3b6e265d22acd692c43544ae3bca20be6028
Agustin Zavalla <azavalla@dc.uba.ar>
2025-06-25 13:22:00 -0300

    WIP

--- src/program/protocols/union/dispute_core.rs
+++ src/program/protocols/union/dispute_core.rs
@@ -64,6 +64,7 @@ impl ProtocolHandler for DisputeCoreProtocol {
     fn generate_keys(
         &self,
         program_context: &mut ProgramContext,
+        program_context: &mut ProgramContext,
     ) -> Result<ParticipantKeys, BitVMXError> {
         let packet_size = self.committee(program_context)?.packet_size;
         let mut keys = vec![];
