d1fb2b960d19b15ebfe51c6405e22a755aae29c9
andrelo86 <andrelovaninetti@gmail.com>
2025-07-23 10:38:04 -0300

    QE-BitVMX: Added support to start multiples operators by sharing the same regtest bitcoind network

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ if [ -z "$CLIENT_OP" ]; then
     echo "❌ CLIENT_OP not specified"
     echo "Usage: ./start.sh <operator> [docker-compose-args]"
     echo "   Examples: ./start.sh op_1 up"
-    echo "             ./start.sh op_2 up -d"
+    echo "             ./start.sh op_2 up -d"  
     echo "             ./start.sh op_3 down"
     exit 1
 fi
@@ -25,14 +25,28 @@ if [ -f "$CONFIG_FILE" ]; then
     echo "   Operator: $CLIENT_OP"
     echo "   Config: $CONFIG_FILE" 
     echo "   Broker Port: $BROKER_PORT"
-    echo "   Container: bitvmx-client-$CLIENT_OP"
+    echo "   Project: bitvmx-$CLIENT_OP"
+    
+    # Set dynamic BITCOIND_URL based on the operator you want to run
+    if [ "$CLIENT_OP" = "op_1" ]; then
+        echo "   Bitcoind: Starting new instance (internal network)"
+        export BITCOIND_URL="http://bitcoind:18443"
+    else
+        echo "   Bitcoind: Using shared instance from op_1 (host network)"
+        export BITCOIND_URL="http://host.docker.internal:18443"
+    fi
+    echo "   Bitcoin URL: $BITCOIND_URL"
     echo ""
     
     export CLIENT_OP="$CLIENT_OP"
     export BROKER_PORT="$BROKER_PORT"
-    export BITVMX_CLIENT_CONTAINER_NAME="bitvmx-client-${CLIENT_OP}"
     
-    exec docker-compose "${@:2}"
+    # Include bitcoind profile only for op_1
+    if [ "$CLIENT_OP" = "op_1" ]; then
+        exec docker-compose --project-name "bitvmx-${CLIENT_OP}" --profile bitcoind "${@:2}"
+    else
+        exec docker-compose --project-name "bitvmx-${CLIENT_OP}" "${@:2}"
+    fi
 else
     echo "❌ Config file $CONFIG_FILE not found"
     exit 1
