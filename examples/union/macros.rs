332ce43df55a7fbb9c8366619da370e63511e20a
H. Andres Tournour <andres.tournour@fairgate.io>
2025-10-16 17:25:14 -0300

    increase macro timeout

--- examples/union/macros.rs
+++ examples/union/macros.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! expect_msg {
     ($bitvmx:expr, $pattern:pat => $expr:expr) => {{
-        let msg = $bitvmx.wait_message(Some(std::time::Duration::from_secs(60)), None)?;
+        let msg = $bitvmx.wait_message(Some(std::time::Duration::from_secs(120)), None)?;
 
         if let $pattern = msg {
             Ok($expr)
