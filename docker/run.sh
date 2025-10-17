591a150941523d4f5ae024a96842996706aa66fa
H. Andres Tournour <andres.tournour@fairgate.io>
2025-10-17 12:08:46 -0300

    Merge branch 'v0.1.2-alpha-fix-build'

--- docker/run.sh
+++ docker/run.sh
@@ -4,8 +4,8 @@ set -e
 OPERATOR="${CLIENT_OP:-op_1}"
 
 if [ -n "$BITCOIND_URL" ]; then
-  sed -i "s|^\([[:space:]]*url:\).*|\1 $BITCOIND_URL|" "/app/rust-bitvmx-workspace/rust-bitvmx-client/config/${OPERATOR}.yaml"
+  sed -i "s|^\([[:space:]]*url:\).*|\1 $BITCOIND_URL|" "/app/config/${OPERATOR}.yaml"
 fi
 
 echo "Starting bitvmx-client with $OPERATOR..."
-exec /app/rust-bitvmx-workspace/rust-bitvmx-client/target/release/bitvmx-client "$OPERATOR"
+exec /app/bitvmx-client "$OPERATOR"
