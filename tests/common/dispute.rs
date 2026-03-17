f611b78e8fd3fb6a5cbc72c946c13b212d2dedd6
Martin Jonas <martinjonas@gmail.com>
2026-03-17 08:48:12 -0300

    use new interface

--- tests/common/dispute.rs
+++ tests/common/dispute.rs
@@ -263,10 +263,12 @@ pub fn execute_dispute(
     let prover_dispatcher = bitvmx_job_dispatcher::DispatcherHandler::<EmulatorJobType>::new(
         emulator_channels[0].clone(),
         instances[0].get_store(),
+        None,
     )?;
     let verifier_dispatcher = bitvmx_job_dispatcher::DispatcherHandler::<EmulatorJobType>::new(
         emulator_channels[1].clone(),
         instances[1].get_store(),
+        None,
     )?;
 
     let mut dispatcher_p = vec![prover_dispatcher];
--- tests/common/helper.rs
+++ tests/common/helper.rs
@@ -423,7 +423,7 @@ fn run_emulator(network: Network, rx: Receiver<()>, tx: Sender<usize>) -> Result
         let storage_path = format!("/tmp/emulator_storage_{i}.db");
         clear_db(&storage_path);
         let prover_dispatcher =
-            DispatcherHandler::<EmulatorJobType>::new_with_path(channel, &storage_path)?;
+            DispatcherHandler::<EmulatorJobType>::new_with_path(channel, &storage_path, None)?;
         instances.push(prover_dispatcher);
     }
 
@@ -465,7 +465,7 @@ fn run_zkp(network: Network, rx: Receiver<()>, tx: Sender<usize>) -> Result<()> 
         let storage_path = format!("/tmp/zkp_storage_{i}.db");
         clear_db(&storage_path);
         let prover_dispatcher =
-            DispatcherHandler::<ProverJobType>::new_with_path(channel, &storage_path)?;
+            DispatcherHandler::<ProverJobType>::new_with_path(channel, &storage_path, None)?;
         instances.push(prover_dispatcher);
     }
 
