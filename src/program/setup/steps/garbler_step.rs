58da6f891a63ac2ffea31818615431d49614feb3
crivasr <camilorivasramunni@gmail.com>
2026-08-25 14:25:35 -0300

    Deserialize GCJobVerifyResult

--- src/program/setup/steps/garbler_step.rs
+++ src/program/setup/steps/garbler_step.rs
@@ -1,7 +1,7 @@
 use crate::ports::bitcoin_coordinator::BitcoinCoordinatorApi;
 use bitvmx_job_dispatcher::dispatcher_job::DispatcherJob;
 use bitvmx_job_dispatcher_types::garbled_messages::{
-    GCJobProveResult, GarbledJobType, ProofBlob, Sha256CommitmentHex,
+    GCJobProveResult, GCJobVerifyResult, GarbledJobType, ProofBlob, Sha256CommitmentHex,
 };
 use bitvmx_settings::settings::decrypt_or_read_file_bytes;
 use key_manager::lamport::{
@@ -217,14 +217,26 @@ impl SetupStep for GarblerStep {
         }
 
         if sub_step == GC_JOB_VERIFY_STEP {
-            info!(" result[\"status\"]: {}", result["status"]);
-            info!(" result[\"type\"]: {}", result["type"]);
-            info!(" result[\"valid\"]: {}", result["valid"]);
-            info!(" result[\"proofs_linked\"]: {}", result["proofs_linked"]);
+            let verify_result: GCJobVerifyResult =
+                serde_json::from_value(result.clone()).map_err(|e| {
+                    BitVMXError::InvalidMessage(format!(
+                        "Failed to deserialize verification data: {}",
+                        e
+                    ))
+                })?;
+
+            info!(" result[\"status\"]: {}", verify_result.status);
+            info!(" result[\"type\"]: {}", verify_result.r#type);
+            info!(" result[\"valid\"]: {}", verify_result.valid);
+            info!(
+                " result[\"proofs_linked\"]: {}",
+                verify_result.proofs_linked
+            );
 
             if !result["valid"].as_bool().unwrap_or(false) {
                 return Err(BitVMXError::InvalidMessage(format!(
-                    "The provided proof is invalid: {result}"
+                    "The provided proof is invalid: {:?}",
+                    verify_result
                 )));
             }
 
